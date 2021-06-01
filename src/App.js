e47cfbeb8d3819c5c6143300fd4a515ad5feaff0
Amiyanshu <amiyanshu2000@gmail.com>
2021-06-01 17:20:01 +0530

    changes theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { render } from "react-dom";
+import events from "./events";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const localizer = momentLocalizer(moment);
+
+const App = () => (
+  <>
+    {/* <div className="circle one"></div> */}
+    {/* <div className="circle two"></div> */}
+    <div style={{ height: 500 }}>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        step={60}
+        defaultDate={new Date(2021, 3, 1)}
+      />
     </div>
-  );
-}
+  </>
+);
 
 export default App;
